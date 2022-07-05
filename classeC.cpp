6c97f3e153e46f9d7b355370a51dd24182862729
lucasnves <lucas.snowsg@gmail.com>
2022-07-05 13:01:07 -0300

    Criação da Classe C

---
+++ classeC.cpp
@@ -0,0 +1,16 @@
+class C {
+    private:
+        string C1;
+        int C2;
+    public:
+        void MC1();
+        void MC2();
+};
+
+void C::MC1 (){
+    cout << "MC1" <<endl;
+}
+
+void C::MC2 (){
+    cout << "MC2" <<endl;
+}
