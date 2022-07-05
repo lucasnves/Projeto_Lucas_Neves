545f19ab18954f619dff4319a2aced03b56680ec
lucasnves <lucas.snowsg@gmail.com>
2022-07-05 13:09:46 -0300

    Criação da Classe D

---
+++ classeD.cpp
@@ -0,0 +1,17 @@
+class D {
+    private:
+        string D1;
+        int D2;
+    public:
+        void MD1();
+        void MD2();
+};
+
+void D::MD1 (){
+    cout << "MD1" <<endl;
+}
+
+void D::MD2 (){
+    cout << "MD2" <<endl;
+}
+
